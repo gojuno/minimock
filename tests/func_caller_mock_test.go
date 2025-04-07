2b6b233db81f13bf29bd05c9ec73cab20c992dad
Ivan Lutsenka <ivlutsenko@ozon.ru>
2025-04-07 20:43:10 +0300

    support func comparison in mocks

---
+++ tests/func_caller_mock_test.go
@@ -0,0 +1,130 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFuncCallerMock_AcceptsFunc(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	validFunc := func() { _ = 1 }
+	mock := NewFuncCallerMock(tester)
+	mock.CallFuncMock.Expect(validFunc).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(validFunc))
+}
+
+func TestFuncCallerMock_ChecksNilFunc(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	var f func()
+	mock := NewFuncCallerMock(tester)
+	mock.CallFuncMock.Expect(f).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(nil))
+}
+
+func TestFuncCallerMock_ChecksNil(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	var f func()
+	mock := NewFuncCallerMock(tester)
+	mock.CallFuncMock.Expect(nil).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(f))
+}
+
+func TestFuncCallerMock_WhenThen(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	var (
+		f1 func()
+		f2 = func() { _ = 2 }
+	)
+	mock := NewFuncCallerMock(tester)
+	mock.
+		CallFuncMock.When(f1).Then(1).
+		CallFuncMock.When(f2).Then(2)
+
+	assert.Equal(t, 1, mock.CallFunc(f1))
+	assert.Equal(t, 2, mock.CallFunc(f2))
+}
+
+func TestFuncCallerMock_Method(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	var obj object
+	mock.
+		CallFuncMock.When(obj.Method1).Then(1).
+		CallFuncMock.When(obj.Method2).Then(2)
+
+	assert.Equal(t, 1, mock.CallFunc(obj.Method1))
+	assert.Equal(t, 2, mock.CallFunc(obj.Method2))
+}
+
+func TestFuncCallerMock_SameVTable(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	var (
+		obj1 object
+		obj2 object
+	)
+	mock.CallFuncMock.Expect(obj1.Method1).Times(2).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(obj1.Method1))
+	assert.Equal(t, 1, mock.CallFunc(obj2.Method1))
+}
+
+func TestFuncCallerMock_Closures(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return().ErrorfMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	mock.CallFuncMock.Expect(func() {}).Return(1)
+
+	_ = mock.CallFunc(func() {})
+}
+
+func TestFuncCallerMock_ClosuresPartial(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return().ErrorfMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	mock.CallFuncMock.ExpectFParam1(func() {}).Return(1)
+
+	_ = mock.CallFunc(func() {})
+}
+
+func TestFuncCallerMock_Partial(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	f := func() {}
+	mock.CallFuncMock.ExpectFParam1(f).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(f))
+}
+
+type object struct{}
+
+func (o object) Method1() {
+}
+
+func (o object) Method2() {
+}
