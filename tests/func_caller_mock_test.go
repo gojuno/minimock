ee36290e683a2ba1e856837971fa7e6599174385
Ivan Lutsenka <ivlutsenko@ozon.ru>
2025-04-08 10:11:01 +0300

    add more tests for FuncCallerMock

--- tests/func_caller_mock_test.go
+++ tests/func_caller_mock_test.go
@@ -121,10 +121,41 @@ func TestFuncCallerMock_Partial(t *testing.T) {
 	assert.Equal(t, 1, mock.CallFunc(f))
 }
 
+func TestFuncCallerMock_Functions(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return().ErrorfMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	mock.CallFuncMock.Expect(function1).Return(1)
+
+	assert.Equal(t, 1, mock.CallFunc(function1))
+	mock.CallFunc(function2)
+}
+
+func TestFuncCallerMock_ClosuresFromFunc(t *testing.T) {
+	tester := NewTesterMock(t)
+	tester.CleanupMock.Return().HelperMock.Return().ErrorfMock.Return()
+
+	mock := NewFuncCallerMock(tester)
+
+	mock.CallFuncMock.Expect(newClosure()).Return(1)
+
+	mock.CallFunc(newClosure())
+}
+
 type object struct{}
 
 func (o object) Method1() {
 }
 
 func (o object) Method2() {
+}
+
+func function1() {}
+
+func function2() {}
+
+func newClosure() func() {
+	return func() {}
 }
